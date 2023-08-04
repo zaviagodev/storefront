2e47e1e1fab6392a64412abc552f1d052b1192f3
MarkSuttirak <suttirak.ch@zaviago.com>
2023-08-04 18:19:25 +0700

    Update NavHeader.jsx

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -29,7 +29,7 @@ const NavHeader = () => {
         onClick: () => setIsOpen(true)
       },
       {
-        icon: (<ShoppingBag01 />),
+        icon: (<ShoppingBag01 viewBox='0 0 24 24' width="22" height="22"/>),
         label: '',
         ariaLabel: 'Cart',
         role: 'button',
