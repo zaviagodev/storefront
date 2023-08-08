209d65d3751515a5e891a8482890a9f57d492e32
Nabeel Goraya <itsgoraya@gmail.com>
2023-08-08 16:10:00 +0500

    Test connection

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,11 +28,9 @@ export default function Login() {
         },
         onSubmit: values => login(values.usr, values.pwd).then(() => navigate("/"))
     });
-
-
     return (
         <form className="p-4 flex gap-4 flex-wrap text-neutral-900 text-start" onSubmit={formik.handleSubmit}>
-            <h2 className="w-full typography-headline-4 md:typography-headline-3 font-bold">Sign in</h2>
+            <h2 className="w-full typography-headline-4 md:typography-headline-3 font-bold">Test - Sign in</h2>
             <label className="w-full flex flex-col gap-0.5">
               <span className="typography-text-sm  font-medium">usr/username</span>
               <SfInput name="usr" type='email' autoComplete="usr" required onChange={formik.handleChange} value={formik.values.usr} />
